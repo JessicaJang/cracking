983f3b1ff22679629f0fd7ce07636a7153d6a4c3
Jessica Jang <wkddbwjd0814@gmail.com>
2018-08-21 17:23:55 -0500

    Uploaded

---
+++ Exercise_7.py
@@ -0,0 +1,7 @@
+#Write a Python expression that combines the string \color{red}{\verb|"Joe Warren
+# is 52 years old."|}"JoeWarrenis52yearsold." from the string 
+# \color{red}{\verb|"Joe Warren"|}"JoeWarren" and the number 5252 and then prints 
+# the result (Hint: Use the function \color{red}{\verb|str|}str to convert the 
+# number into a string.)
+
+print ('Joe Warren is',str(52),'years old')
