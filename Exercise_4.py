4e193b60eadf95a6d1252809854d4f9f8e5c692a
Jessica Jang <wkddbwjd0814@gmail.com>
2018-08-21 14:57:45 -0500

    Uploaded

---
+++ Exercise_4.py
@@ -0,0 +1,6 @@
+#Given p dollars, the future value of this money when compounded yearly 
+#at a rate of rr percent interest for yy years is p (1 + 0.01 r)^yp(1+0.01r) y
+#Write a Python statement that calculates and prints the value of 10001000 
+#dollars compounded at 77 percent interest for 1010 years.
+
+print('future value of 1000 dollars = ',1000*((1+0.01*7)**10))
