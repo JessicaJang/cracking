42c309f39a98aefc757ffc6cb65cdfd2902039b1
Jessica Jang <wkddbwjd0814@gmail.com>
2018-08-22 12:46:18 -0500

    uploaded

---
+++ 082218_2.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+def print_digit(x):
+	ten = x // 10
+	rest = x % 10
+	
+	print('The tens digit is '+str(ten)+', and the ones digit is '+str(rest)+'.')
+
+
+print_digit(43)
+
