e8f2e7e33edda02497f722a9729eec31910d7b48
Jessica Jang <wkddbwjd0814@gmail.com>
2018-08-23 15:21:51 -0500

    uploaded

---
+++ 082318_1.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+def powerball():
+	
+	print("Today's number are "+str(random.randrange(1,60))+", "+
+		str(random.randrange(1,60))+", "+
+		str(random.randrange(1,60))+", "+
+		str(random.randrange(1,60))+", and "+
+		str(random.randrange(1,60))+". "+
+		"The Powerball number is "+str(random.randrange(1,36))+".")
+
+	return
+
+powerball()
+powerball()
+powerball()
+
+
