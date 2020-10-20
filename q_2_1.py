8c5e6c03d8912299f4ac3f96d530643fa97bfc02
Jessica Jang <jessica.youjeong@gmail.com>
2020-10-19 22:29:23 -0500

    q_2_1.py solved

--- q_2_1.py
+++ q_2_1.py
@@ -3,3 +3,39 @@
 # Write code to remove duplicates from an unsorted linked list
 
 import os
+
+class ListNode(object):
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+def remove_dups(head):
+	if head is None:
+		return
+
+	current = head
+	hashMap = set([current.val])
+
+	while current.next:
+		if current.next.val in hashMap:
+			current.next = current.next.next
+		else:
+			hashMap.add(current.next.val)
+			current = current.next
+	return head
+
+head = ListNode(20)
+head.next = ListNode(70)
+head.next.next = ListNode(10)
+head.next.next.next = ListNode(70)
+
+ans = remove_dups(head)
+track = head
+while track.next:
+	print(track.val)
+	track = track.next
+print(track.val)
+
+
+
+
