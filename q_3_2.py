1506a0bf06b2d7e3d41f7a96dfe60e07a5a05ebe
Jessica Jang <jessica.youjeong@gmail.com>
2020-11-15 22:46:35 -0600

    q_3_2.py

---
+++ q_3_2.py
@@ -0,0 +1,65 @@
+# Chapter 3 Stacks and Queues
+# Interview Questions 3.2
+
+# How would you design a stack which, in addition to push adn pop, 
+# has a function min, which returns the minimum element?
+# Push, pop and min should all operate in O(1) time
+
+class Stack:
+	def __init__(self, n):
+		self.arr = []
+		self.minVal = 0
+		self.n = n
+
+	def isEmpty(self):
+		if len(self.arr) == 0:
+			return True
+		return False
+
+	def isFull(self):
+		if self.n == len(self.arr):
+			return True
+		return False
+
+	def pop(self):
+		if self.isEmpty():
+			self.minVal = 0
+			return None
+
+		tmp = self.arr.pop()
+		if tmp <= self.minVal:
+			self.minVal = 2*tmp - self.minVal
+		return tmp
+
+
+	def push(self, item):
+		if self.isEmpty():
+			self.minVal = item
+
+		if self.isFull():
+			print("Stack is Full")
+			return
+
+		if item <= self.minVal:
+			tmp = self.minVal
+			self.minVal = item
+			item = 2*item - self.minVal
+			
+		self.arr.append(item)
+
+
+	def getMin(self):
+		return self.minVal
+		
+if __name__ == "__main__":
+	stack = Stack(10)
+	stack.push(3)
+	stack.push(5)
+	print(stack.getMin())
+	stack.push(2)
+	stack.push(1)
+	print(stack.getMin())
+	stack.pop()
+	print(stack.getMin())
+	stack.pop()
+	print(stack.getMin())
